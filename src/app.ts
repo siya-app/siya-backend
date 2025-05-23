27f6a5dc2a65efebf8bd52d7a2cf4b92f531884a
OhMyLaia <ohmylaia@live.com>
2025-05-23 17:43:50 +0200

    created object random to test database -- everything ok

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
+
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow only your frontend // 5173 for vite app
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    credentials: true // If you need cookies/auth headers!
+    //TODO take a look at middleware headers auth
+}));
+
+//This middleware parses incoming JSON data from the request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // For form data
+
+// api routes here
+
+// middleware -->
+// app.use(notFound);
+// app.use(handleError);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
---
+++ src/model/config/sequelize-config.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.MYSQL_HOST || 'localhost',
+        dialect: 'mysql',
+    }
+)
