6f346f295b262b4dac75ba1764ac3b52305ea301
OhMyLaia <ohmylaia@live.com>
2025-05-26 21:52:46 +0200

    WIP: pagination function

--- src/services/terrace.service.ts
+++ src/services/terrace.service.ts
@@ -2,6 +2,13 @@ import { createApiInstance } from '../config/api-connection-config.js'
 import { axiosRequest } from '../config/api-connection-service.js';
 import { ENV } from '../config/env.js';
 
+type PaginatedResult<T> = {
+    data: T[];
+    next: NextFunction<T> | null;
+};
+
+type NextFunction<T> = () => Promise<PaginatedResult<T> | null>;
+
 
 const TERRACE_API: string = ENV.TERRACE_API_URL || "";
 
@@ -14,24 +21,25 @@ const apiTerrace = createApiInstance(TERRACE_API);
 export async function fetchDataTerraces(): Promise<object[]> {
 
     try {
-        const response = await axiosRequest(apiTerrace, TERRACE_API, {limit: 3});
+        const response = await axiosRequest(apiTerrace, TERRACE_API, { limit: 3 });
 
         // if (!response || !response.result) {
         //     console.log("❌ API terrace response was null or invalid");
         //     return [];
         // }
-        
+        const totalLength = response?.result?.total;
+
         const records = response?.result?.records;
         if (!records || records.length === 0) {
             console.log("❌ No terrace records found");
             // return []
         }
 
-        console.warn("✅🍸 Received data from api terraces,", records.length);
+        console.log("✅🍸 Received data from api terraces,", records.length, "total length:", totalLength);
         // console.log("✅ Received terraces data.records:", records);
         return records;
 
-    } catch(error) {
+    } catch (error) {
         console.log("❌ Error fetching terraces, error:", error);
         return [];
     }
