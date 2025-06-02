2cd34fece3c16823c7ba45bdd0b26b060d6b88c9
OhMyLaia <ohmylaia@live.com>
2025-06-02 12:49:30 +0200

    changed some module paths

--- src/services/terrace-services/mockData/mock.services.ts
+++ src/services/terrace-services/mockData/mock.services.ts
@@ -1,11 +1,11 @@
-import * as {businesses } from './mock.business.js'
-import 
+// import * as {businesses } from './mock.business.js'
+// import 
 
-function fetchMockData() {
+// function fetchMockData() {
 
-    fetch("../services/contributors.JSON")
-    .then(res => res.json())
-    .then(data => console.log(data))
+//     fetch("../services/contributors.JSON")
+//     .then(res => res.json())
+//     .then(data => console.log(data))
 
-}
+// }
 
