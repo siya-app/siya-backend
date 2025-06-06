ed7c35d7b96db027545a3d01d2869a730c48a1f3
Mika7596 <mika7596@gmail.com>
2025-06-06 11:33:39 +0200

    Created User Model

--- src/models/user-model/user.model.ts
+++ src/models/user-model/user.model.ts
@@ -1,4 +1,4 @@
-// models/user.model.ts
+
 import { Model, DataTypes } from 'sequelize';
 import { sequelize } from '../../config/sequelize-config.js';
 
@@ -16,24 +16,38 @@ User.init(
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
+      primaryKey: true,
+      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
+      validate : {
+          isEmail : true
+      }
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    age: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
+    birth_date : {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    password_hash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+    type: DataTypes.ENUM('client', 'owner'), 
+    defaultValue: 'client',
+    allowNull: false,
+  }
   },
   {
     sequelize,
+    modelName: 'User',
     tableName: 'users',
     timestamps: true
   }
