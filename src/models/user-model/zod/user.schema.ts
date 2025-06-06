0b83f0c0e0fa68a1af44939be95a4d62f3a048bc
Mika7596 <mika7596@gmail.com>
2025-06-06 11:43:09 +0200

    Created User schema.

--- src/models/user-model/zod/user.schema.ts
+++ src/models/user-model/zod/user.schema.ts
@@ -3,7 +3,14 @@ import { z } from 'zod';
 export const userSchema = z.object({
   email: z.string().email(),
   name: z.string().min(1),
-  age: z.number().int().positive("La edad debe ser un número entero positivo.")
+  password_hash: z.string()
+    .min(8)
+    .regex(/[A-Z]/)
+    .regex(/[a-z]/)
+    .regex(/[0-9]/)
+    .regex(/[^a-zA-Z0-9]/),
+  birth_date: z.string()
+    .datetime()
 });
 
 export type UserSchema = z.infer<typeof userSchema>;
