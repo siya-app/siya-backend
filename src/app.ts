739846b0e8ca617e7eeabce12194188a92529992
unknown <ronnievelz@gmail.com>
2025-06-10 11:34:15 +0200

    solved merged problem

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,9 @@ import uploadRoutes from './routes/upload-routes/upload.route.js'
 import { createCustomValidatedTerrace } from './controllers/terrace-controllers/terrace.validator.js';
 import terraceRoutes from './routes/terrace-routes/terraces.router.js';
 import userRoutes from './routes/user-routes/user.routes.js'
-import bookingRoutes from './routes/booking-routes/booking.routes.js'
 import { sequelize } from './config/sequelize-config.js';
 import paymentRoutes from "./routes/payment-routes/payment.route.js"
+import bookingRoutes from './routes/booking-routes/booking.routes.js'
 
 console.log('--- STARTUP TEST LOG ---');
 
@@ -72,9 +72,8 @@ app.use(express.urlencoded({ extended: true, limit:"50mb" })); // For form data
 // api routes here
 app.use('/',userRoutes);
 app.use('/', terraceRoutes);
-app.use("/", bookingRoutes )
-
-app.use("/", paymentRoutes);
+app.use('/', bookingRoutes)
+app.use('/', paymentRoutes)
 app.use('/', uploadRoutes)
 
 // middleware -->
