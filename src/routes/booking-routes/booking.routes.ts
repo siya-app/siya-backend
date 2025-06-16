c14688581ac49c9b6b67a9b9ccbe350988a35bf4
RonniedeTal <ronnievelz@gmail.com>
2025-06-16 13:04:44 +0200

    updated model

--- src/routes/booking-routes/booking.routes.ts
+++ src/routes/booking-routes/booking.routes.ts
@@ -1,13 +1,13 @@
 import { Router} from "express"
 import { createBooking, deleteBookingById, getBooking, getBookingById, updateBooking } from "../../controllers/booking-controllers/booking.controller.js"
-
+import { isTokenValid } from "../../middleware/auth.middleware.js"
 
 const router=Router()
 
-router.post("/booking", createBooking)
-router.put("/booking/:id", updateBooking)
-router.get("/booking", getBooking )
-router.get("/booking/:id", getBookingById )
-router.get("/booking/id", deleteBookingById )
+router.post("/booking",isTokenValid, createBooking)
+router.put("/booking/:id",isTokenValid, updateBooking)
+router.get("/booking",isTokenValid, getBooking )
+router.get("/booking/:id",isTokenValid, getBookingById )
+router.get("/booking/id",isTokenValid, deleteBookingById )
 
 export default router
