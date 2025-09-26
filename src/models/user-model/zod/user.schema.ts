d5efb3e7644e9425e43a177eda330ec549df03ed
Mika7596 <mika7596@gmail.com>
2025-09-26 19:11:40 +0200

    fixed an error in user.schema.ts

--- src/models/user-model/zod/user.schema.ts
+++ src/models/user-model/zod/user.schema.ts
@@ -4,7 +4,8 @@ export const userSchema = z.object({
   email: z.string().email(),
   name: z.string().min(2),
   password_hash: z.string().min(8),
-  birth_date: z.string()
+  birth_date: z.string(),
+  role: z.enum(["client", "owner"]).optional()
 });
 
 export type UserSchema = z.infer<typeof userSchema>;
